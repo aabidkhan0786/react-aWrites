482a92fad41560abd8d74e257a62dc6ab18c8d2f
aabidkhan0786 <62328527+aabidkhan0786@users.noreply.github.com>
2020-08-17 21:33:59 +0530

    Updates

---
+++ precache-manifest.bb2960372ddfbf4c1944ae3e24a5ac9c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa6a2b47a29247e7672ecd3a211cdd02",
+    "url": "/react-iWrites/index.html"
+  },
+  {
+    "revision": "445a0e5000a2fc624afd",
+    "url": "/react-iWrites/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "dfcce32953de3714ab50",
+    "url": "/react-iWrites/static/css/main.339707d1.chunk.css"
+  },
+  {
+    "revision": "445a0e5000a2fc624afd",
+    "url": "/react-iWrites/static/js/2.291d7ae2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-iWrites/static/js/2.291d7ae2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfcce32953de3714ab50",
+    "url": "/react-iWrites/static/js/main.67a5cbca.chunk.js"
+  },
+  {
+    "revision": "598c8c18e598fca9c253",
+    "url": "/react-iWrites/static/js/runtime-main.2f0a76c8.js"
+  },
+  {
+    "revision": "38693431ae5fcc8e5843392124348d1f",
+    "url": "/react-iWrites/static/media/logo2.38693431.png"
+  },
+  {
+    "revision": "3d8d6fd3cac9bf55add41bf963ec341a",
+    "url": "/react-iWrites/static/media/thanks1.3d8d6fd3.gif"
+  }
+]);
