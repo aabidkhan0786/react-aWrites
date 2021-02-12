039bcfac2bada9109757b327d19c8b77045d6359
Abdul Aabid Khan <62328527+aabidkhan0786@users.noreply.github.com>
2021-02-12 21:20:47 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -11,7 +11,7 @@ const Header =()=>{
             <nav class="navbar navbar-dark bg-dark">
             <a class="navbar-brand" href="/">
                 <img src={logo2} width="40" height="40" class="d-inline-block align-top" alt="logo" loading="lazy"/>
-                    <span className="head pl-3 py-auto"><u>iWrites</u></span>
+                    <span className="head pl-3 py-auto"><u>aWrites</u></span>
                 </a>  
  
             </nav>
